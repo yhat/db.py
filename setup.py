3b0b27181c39ddaed62d0be8579a295f7b84136e
glamp <lamp.greg@gmail.com>
2014-11-05 22:09:52 -0500

    bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ required = [
 
 setup(
     name="db.py",
-    version="0.2.5",
+    version="0.2.6",
     author="Greg Lamp",
     author_email="greg@yhathq.com",
     url="https://github.com/yhat/db.py",
