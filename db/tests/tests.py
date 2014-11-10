8387fee1593ec5b4477d93d20286324178a27fd3
glamp <lamp.greg@gmail.com>
2014-11-09 20:04:08 -0500

    some basic tests

--- db/tests/tests.py
+++ db/tests/tests.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from db import DemoDB, DB
+from db import DemoDB, DB, list_profiles, remove_profile
 import unittest
 
 
@@ -15,40 +15,58 @@ class PandaSQLTest(unittest.TestCase):
         self.assertEqual(len(df), 275)
     
     def test_query_groupby(self):
-        pass
+        q = "select AlbumId, sum(1) from Track group by 1"
+        df = db.query(q)
+        self.assertEqual(len(df), 347)
 
     def test_query_from_file_rowsum(self):
-        pass
+        with open("/tmp/testscript.sql", "w") as f:
+            f.write("select * from Artist;")
+        df = db.query_from_file("/tmp/testscript.sql")
+        self.assertEqual(len(df), 275)
 
     def test_add_profile(self):
-        pass
+        profiles = list_profiles()
+        db.save_credentials(profile="test_profile")
+        self.assertEqual(len(profiles)+1, len(list_profiles()))
+        remove_profile("test_profile")
     
     def test_remove_profile(self):
-        pass
+        profiles = list_profiles()
+        db.save_credentials(profile="test_profile")
+        self.assertEqual(len(profiles)+1, len(list_profiles()))
+        remove_profile("test_profile")
 
     def test_list_profiles(self):
-        pass
+        db.save_credentials(profile="test_profile")
+        self.assertTrue(len(list_profiles()) > 0)
+        remove_profile("test_profile")
 
     def test_table_head(self):
-        pass
+        self.assertEqual(len(db.tables.Artist.head()), 6)
 
     def test_table_all(self):
-        pass
+        self.assertEqual(len(db.tables.Artist.all()), 275)
 
     def test_table_select(self):
-        pass
+        df = db.tables.Artist.select("ArtistId", "Name")
+        self.assertEqual(df.shape, (275, 2))
 
     def test_table_sample(self):
-        pass
+        df = db.tables.Artist.sample(n=10)
+        self.assertEqual(len(df), 10)
 
     def test_column_head(self):
-        pass
+        col = db.tables.Track.TrackId.head()
+        self.assertEqual(len(col), 6)
 
     def test_column_all(self):
-        pass
+        col = db.tables.Track.TrackId.all()
+        self.assertEqual(len(col), 3503)
 
     def test_column_sample(self):
-        pass
+        col = db.tables.Track.TrackId.sample(n=10)
+        self.assertEqual(len(col), 10)
 
 
 if __name__=="__main__":
